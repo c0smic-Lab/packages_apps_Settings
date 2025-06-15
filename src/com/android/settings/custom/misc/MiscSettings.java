clarencekopitiam/android_packages_apps_Settings

Settings: Add option to toggle Google Play Integrity spoofing [2/2]
Signed-off-by: clarencelol <clarencekuiek@proton.me>

src/com/android/settings/pixelage/misc/MiscSettings.java
@@ -1,0 +1,22 @@
+package com.android.settings.pixelage.misc;
+
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class MiscSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.misc_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.PIXELAGE_HOUSE;
+    }
+}

c0smic-Lab/packages_apps_Settings

src/com/android/settings/custom/misc/MiscSettings.java
@@ -1,0 +1,22 @@
+package com.android.settings.custom.misc;
+
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class MiscSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.misc_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM;
+    }
+}